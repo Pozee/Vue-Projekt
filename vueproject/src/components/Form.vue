8a5912325ca98c5845474365f6246ef1b150157c
alexandrajampolskaia <a.jampolskaia@gmail.com>
2020-04-07 23:41:29 +0200

    added form and submit function

--- vueproject/src/components/Form.vue
+++ vueproject/src/components/Form.vue
@@ -1,11 +1,56 @@
 <template>
   <div>
-    <h1>Alex</h1>
+    <form v-if="!show">
+      <div>
+        <h1>BREAK MANAGER</h1>
+
+        <div>
+          <label>Task</label> &nbsp;
+          <input class="task" type="text" v-model="task" />
+        </div>
+
+        <div>
+          <label>Description of task</label> &nbsp;
+          <input class="description" type="text" v-model="description" />
+        </div>
+
+        <div>
+          <label>Estimated time to execute (h)</label> &nbsp;
+          <input class="time" type="number" v-model="time" />
+        </div>
+      </div>
+
+      <button @click="addTask">Add Task</button>
+    </form>
+
+    <div v-if="show">Your task is to: <a>{{ task }}</a> <br>
+	Description: <a>{{ description }}</a> <br> 
+	The estimated execturion time is: <a>{{ time }}</a> hours!
+	<button></button></div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Form",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      task: "",
+      description: "",
+      time: "",
+      show: false
+    };
+  },
+  methods: {
+    addTask() {
+      this.show = true;
+	},
+	
+  }
+};
 </script>
 
 <style>
