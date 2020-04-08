68ef60a0904712b6af1e460ca7c5a9ecbe6615b6
alexandrajampolskaia <a.jampolskaia@gmail.com>
2020-04-08 16:35:20 +0200

    added new header component

--- vueproject/src/App.vue
+++ vueproject/src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-	<Form />
+    <HeaderTimer />
+    <br><Form />
   </div>
 </template>
 
 <script>
-import Form from './components/Form.vue'
+import HeaderTimer from "./components/HeaderTimer.vue";
+import Form from "./components/Form.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-	Form
+    HeaderTimer,
+    Form
   }
-}
+};
 </script>
 
 <style>
@@ -22,6 +25,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
---
+++ vueproject/src/components/HeaderTimer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+<div class="header"></div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderTimer",
+  props: {
+    msg: String
+  },
+}
+</script>
+
+<style>
+.header{
+	margin-top: 0;
+	width: 100vw;
+	height: 100px;
+	background: lightcyan;
+}
+</style>
