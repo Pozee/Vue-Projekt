c5ae06f9b65010dde44034dd1b2df7b41ceb0f07
Sasko89 <nekada89@gmail.com>
2020-04-09 11:29:58 +0200

    commit test

--- vueproject/src/coronaAPI.vue
+++ vueproject/src/coronaAPI.vue
@@ -1,23 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="coronaAPI">
+    <h1>hej</h1>
+    <h1>corona</h1>
+    <h1>hejdå</h1>
+    <h1>corona</h1>
+    <h1>hej</h1>
+    <h1>sommar!</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'coronaAPI',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
+#coronaAPI {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
