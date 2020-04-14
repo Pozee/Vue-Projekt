0aad0e23f3f289097422e5486502bab15e3c1a69
Pontus <kontakt@pontuszetterberg.com>
2020-04-14 09:36:05 +0200

    fixed axios

--- vueproject/src/main.js
+++ vueproject/src/main.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	render: h => h(App)
+}).$mount("#app");
